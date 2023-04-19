ba4e8214c81b5c0143d119659aaaf9ebe0d02f67
getxd <75668448+getxd-dev@users.noreply.github.com>
2023-04-19 13:05:20 +0800

    update bypass url

--- settings.example.js
+++ settings.example.js
@@ -52,7 +52,7 @@ export default {
     chatGptBrowserClient: {
         // (Optional) Support for a reverse proxy for the conversation endpoint (private API server).
         // Warning: This will expose your access token to a third party. Consider the risks before using this.
-        reverseProxyUrl: 'https://bypass.churchless.tech/api/conversation',
+        reverseProxyUrl: 'https://ai.fakeopen.com/api/conversation',
         // Access token from https://chat.openai.com/api/auth/session
         accessToken: '',
         // Cookies from chat.openai.com (likely not required if using reverse proxy server).
